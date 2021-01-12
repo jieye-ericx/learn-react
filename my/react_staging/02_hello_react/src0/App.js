a7597084a9cea3ccf78318ca566acea8f1f2926d
radoapx-MBA <radoapx@gmail.com>
2021-01-12 12:29:28 +0800

    20210112 update ignore

---
+++ my/react_staging/02_hello_react/src0/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
